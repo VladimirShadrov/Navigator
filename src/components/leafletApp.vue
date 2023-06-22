a5a31da21a04c9c29ef53532ad1b42798f9c0696
Vladimir <shadrov_vladimir@mail.ru>
2023-06-22 14:57:27 +0300

    Добавлена карта

--- src/components/leafletApp.vue
+++ src/components/leafletApp.vue
@@ -1,11 +1,31 @@
-<template></template>
+<template>
+  <div id="leaflet"></div>
+</template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 
 export default defineComponent({
   name: 'leaflet-app',
+  mounted() {
+    new L.Map('leaflet', {
+      center: new L.LatLng(55.754142, 37.620212),
+      zoom: 15,
+      layers: [new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')],
+    });
+  },
 });
 </script>
 
-<style></style>
+<style>
+#leaflet {
+  width: 100%;
+  height: 100%;
+  position: relative;
+}
+.leaflet-bottom.leaflet-right {
+  display: none;
+}
+</style>
--- src/components/mapApp.vue
+++ src/components/mapApp.vue
@@ -1,19 +1,25 @@
 <template>
-  <div class="map"></div>
+  <div class="map">
+    <leaflet-app />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import leafletApp from './leafletApp.vue';
 
 export default defineComponent({
   name: 'map-app',
+  components: {
+    leafletApp,
+  },
 });
 </script>
 
 <style scoped>
 .map {
-  background: purple;
-  height: 500px;
+  height: 800px;
   width: 100%;
+  overflow: hidden;
 }
 </style>
