bcecfd92c402dbb775e49ab9b511d79c83c9b738
Vladimir <shadrov_vladimir@mail.ru>
2023-06-22 12:00:54 +0300

    Старт проекта

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  setup() {
+    const title = 'Новое приложение на Vue 3';
+
+    return {
+      title,
+    };
+  },
+});
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
